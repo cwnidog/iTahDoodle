6287e43f4d81f2978f2136b03c7a4b2452780dde
cwnidog <cwnidog@comcast.net>
2015-08-21 15:56:30 -0700

    Connecting up insert button and table view for task list.

--- iTahDoodle/AppDelegate.m
+++ iTahDoodle/AppDelegate.m
@@ -16,6 +16,10 @@
 
 #pragma mark - Application delegate callbacks
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  // create an empty tasks array to get started
+  self.tasks = [NSMutableArray array];
+  
+  // set up screen
   CGRect winFrame = [[UIScreen mainScreen] bounds];
   UIWindow *theWindow = [[UIWindow alloc] initWithFrame:winFrame];
   self.window = theWindow;
@@ -29,6 +33,9 @@
   self.taskTable = [[UITableView alloc] initWithFrame:tableFrame style:UITableViewStylePlain];
   self.taskTable.separatorStyle = UITableViewCellSeparatorStyleNone;
   
+  // make AppDelegate the taskTable's data source
+  self.taskTable.dataSource = self;
+  
   // Tell table what class to instantiate when it needs to create a new cell
   [self.taskTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
   
@@ -41,6 +48,9 @@
   self.insertButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
   self.insertButton.frame = buttonFrame;
   [self.insertButton setTitle:@"Insert" forState:UIControlStateNormal];
+  
+  // Set target and action for Insert button
+  [self.insertButton addTarget:self action:@selector(addTask:) forControlEvents:UIControlEventTouchUpInside];
   
   // add the three elemants to the window
   [self.window addSubview:self.taskTable];
@@ -75,5 +85,48 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+#pragma mark - Actions
+- (void)addTask:(id)sender
+{
+  // get the task
+  NSString * text = [self.taskField text];
+  
+  // quit if taskField is empty
+  if([text length] == 0)
+  {
+    return;
+  } // empty text
+  
+  // add the task to the table
+  [self.tasks addObject:text];
+  
+  // refresh the table to show th enew entry
+  [self.taskTable reloadData];
+  
+  // clear the text field and dismiss the keyboard
+  [self.taskField setText:@""];
+  [self.taskField resignFirstResponder];
+} // addTask()
+
+#pragma mark - Table view management
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+  // only has one section, so can just return the number of tasks
+  return [self.tasks count];
+} // numberOfRowsInSection
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  // check to see if there's an existing cell object that can be reused
+  UITableViewCell *c = [self.taskTable dequeueReusableCellWithIdentifier:@"Cell"];
+  
+  // (re)configure cell based on the model object
+  NSString *item = [self.tasks objectAtIndex:indexPath.row];
+  c.textLabel.text = item;
+  
+  return c;
+} // cellForRowAtIndexPath
+
 
 @end
